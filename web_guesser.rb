00c5f0f1e4f2e662a0eb2e2c407e25e483ec7ece
Tim Yan <tyan2@buffalo.edu>
2017-01-15 10:08:52 +0000

    number guessing game logic complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,15 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = rand(101)
+SECRET_NUMBER = rand(101)
 message = "too high!"
 
 
 get '/' do
   guess = params["guess"].to_i
-  if guess > secret_number
-    "too high!"
+  message = check_guess(guess)
+  erb :index, :locals => { :number => SECRET_NUMBER,  :message => message }
+end
+
+def check_guess(guess)
+  if guess.nil?
+    "enter a number"
+  elsif guess > SECRET_NUMBER + 5
+    "way too high"
+  elsif guess < SECRET_NUMBER - 5
+    "way too low"
+  elsif guess > SECRET_NUMBER
+    "too high"
+  elsif guess < SECRET_NUMBER
+    "too low"
+  elsif guess == SECRET_NUMBER
+    "correct, the secret number is #{SECRET_NUMBER}"
   end
-  
-  erb :index, :locals => { :secret_number => secret_number,  :message => message, :guess => guess }
 end
